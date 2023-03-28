248025f1a06b3e7aa92240a32a7be187e8e311fc
Angela <amr25@berkeley.edu>
2023-03-28 00:15:35 -0700

    UI?

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -1,13 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemCollector : MonoBehaviour
 {
-    //private void OnTriggerEnter2D(Collider2D collision)
-    //{
+    public GameObject popup;
+    private Animator anim;
+    private int items = 0;
 
-    //}
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Item"))
+        {
+            
+            items++;
+            popup.SetActive(true);
+            //Text();
+            
+        }
+    }
+    /**
+    private void Text()
+    {
+        anim.SetTrigger("collect");
+    }
+    */
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Item")) 
+        {
+            popup.SetActive(false);
+            Destroy(other.gameObject);
+        }
+    }
 
     
 }
