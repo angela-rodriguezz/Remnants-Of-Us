2a20d53c55919c2181e61d26e21bbefca8af0ea9
viviennelin123 <viviennelin@berkeley.edu>
2023-04-03 23:33:41 -0700

    Added press button to skip dialogue functionality
    - added to ChatBox.cs: press "V" key to stop typing animation simulation and display all dialogue at once
    - it sort of works but it's not consistent so you have to spam V sometimes

--- Assets/Scripts/ChatBox.cs
+++ Assets/Scripts/ChatBox.cs
@@ -31,6 +31,7 @@ public class ChatBox : MonoBehaviour
     // Start is called before the first frame update
     public void PlayNextSentence()
     {
+        state = State.PLAYING;
         StartCoroutine(TypeText(currentScene.sentences[++sentenceIndex].text));
         personNameText.text = currentScene.sentences[sentenceIndex].speaker.speakerName;
         personNameText.color = currentScene.sentences[sentenceIndex].speaker.textColor;
@@ -43,7 +44,7 @@ public class ChatBox : MonoBehaviour
 
     public bool IsCompleted()
     {
-        return state == State.COMPLETED;
+        return state == State.COMPLETED; 
     }
 
     public bool IsLastSentence()
@@ -54,18 +55,35 @@ public class ChatBox : MonoBehaviour
     private IEnumerator TypeText(string text)
     {
         barText.text = "";
-        state = State.PLAYING;
+        //state = State.PLAYING;
         int wordIndex = 0;
 
         while (state != State.COMPLETED)
         {
             barText.text += text[wordIndex];
             yield return new WaitForSeconds(0.05f);
-            if (++wordIndex == text.Length)
+            if (Input.GetKeyDown(KeyCode.V)) //|| Input.GetMouseButtonDown(0)) //if player presses left click, skip to completed dialogue Input.GetMouseButtonDown(0) Input.GetKey(KeyCode.Mouse0)
+            {
+                barText.text = text;
+                state = State.COMPLETED;
+                Debug.Log("skip dialogue");
+                break;
+            }
+            else if (++wordIndex == text.Length)
             {
                 state = State.COMPLETED;
                 break;
             }
+            //yield return new WaitForSeconds(0.05f);
+            /*
+            else if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) //if player presses left click, skip to completed dialogue Input.GetMouseButtonDown(0) Input.GetKey(KeyCode.Mouse0)
+            {
+                barText.text = text; 
+                state = State.COMPLETED;
+                Debug.Log("skip dialogue");
+                break;
+            }
+            */
         }
     }
 }
